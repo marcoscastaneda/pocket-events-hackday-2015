81a9f858006a0841b75117608d63fb2e27120336
mcast323 <mcast323@fiu.edu>
2015-07-15 19:19:17 -0500

    Add Event table

---
+++ Hack Day/AddEventTableView.swift
@@ -0,0 +1,31 @@
+//
+//  AddEventTableView.swift
+//  Hack Day
+//
+//  Created by Marcos Castaneda on 7/15/15.
+//  Copyright (c) 2015 State Farm. All rights reserved.
+//
+
+import UIKit
+
+class AddEventTableView: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.title = "Add Event"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
