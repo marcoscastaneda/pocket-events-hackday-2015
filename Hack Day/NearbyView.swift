5c06e9e1f7222a931875427f0d3e039ece50c9c2
Leonardo Martin <lmart296@fiu.edu>
2015-07-15 22:45:37 -0500

    I'm a beast - my location

--- Hack Day/NearbyView.swift
+++ Hack Day/NearbyView.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class NearbyView: UIViewController {
+class NearbyView: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
     
-    let regionRadius: CLLocationDistance = 4000
+    let regionRadius: CLLocationDistance = 400000
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
             regionRadius * 2.0, regionRadius * 2.0)
@@ -22,15 +25,58 @@ class NearbyView: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
         // set initial location in Honolulu
         let initialLocation = CLLocation(latitude:40.493056, longitude:-88.988333)
         
+        mapView.showsUserLocation = true
         centerMapOnLocation(initialLocation)
         
         // Do any additional setup after loading the view.
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude:self.mapView.userLocation.coordinate.latitude, longitude:self.mapView.userLocation.coordinate.longitude)
+        self.mapView.addAnnotation(annotation)
+    }
+    
+    func locationManager(manager:CLLocationManager!, didUpdateLocations locations:[AnyObject]!){
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: { (placemarks, error) -> Void in
+            
+            if error != nil {
+                println("Error" + error.localizedDescription)
+                return
+            }
+        
+            if placemarks.count > 0
+            {
+                let pm = placemarks[0] as! CLPlacemark
+                self.displayLocationInfo(pm)
+            }
+            
+        })
     }
 
+    
+    func displayLocationInfo(placemark: CLPlacemark){
+        
+        self.locationManager.stopUpdatingLocation()
+        println(placemark.locality)
+        println(placemark.postalCode)
+        println(placemark.administrativeArea)
+        println(placemark.country)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println("Error: " + error.localizedDescription)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
